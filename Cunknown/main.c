71e2ae3f6aad80f74eb392e8bf2de34906648068
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2019-12-24 10:30:40 +0100

    main OMP

--- Cunknown/main.c
+++ Cunknown/main.c
@@ -1,7 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <omp.h>
 
 #include "fonctions.h"
+
+
+void do_task(u64 current, struct task_t *task, const u64 *X)
+{
+    u64 W0 = current >> known_low;
+    u64 WC = current % (1 << known_low);
+    printf("Doing task %lld / %lld\n", W0, WC);
+    
+    prepare_task(X, W0, WC, task);
+    for (u64 r = 0; r < 1 << (nbiter * known_up); r++) {
+        task->rot[0] = (task->rot[0] + 1) % k;
+        int i = 0;
+        while (task->rot[i] == 0 && i < nbiter) {
+            i++;
+            task->rot[i] = (task->rot[i] + 1) % k;
+        }
+            
+        if (solve_isgood(task)) {
+            u64 DS640;
+            u64 Y0;
+            solve(task, &DS640, &Y0);
+
+            printf("thread %d, candidat DS64 trouvé !! %lld\n", omp_get_thread_num(), DS640);
+        }
+    }
+    finish_task(X, task);
+}
+
 
 int main()
 {
@@ -14,45 +43,42 @@ int main()
     pcg128_t c = ((((pcg128_t) 6364136223846793005u) << k) + 1442695040888963407u) >> 1;
     pcg128_t vraiS[nboutput];
     pcg(vraiS, X, S0, &c, nboutput);
+    
+    // restart();
+
+    u64 range_start = (5018 << known_low) + 335;
+    u64 range_end = range_start + 10;
 
     double t1 = wtime();
-    u64 W0 = 5018, WC = 335;
 
-    /* either one task per thread, or we paralellize the "r" loop below */
-    struct task_t task;
-    init_task(&task);
-
-    for (W0 = 5018; W0 < /*(1<<known_low)*/ 5019 ; W0++){//W0=5018 
-        for (WC = 335 ; WC < /*(1<<known_low)*/ 336 ; WC++){//WC = 335
-            
-            prepare_task(X, W0, WC, &task);
-
-            for(int r = 0 ; r < 1<<(nbiter * known_up) ; r++) {
-                
-                /***** Modification de rot et unrotX *****/
-                task.rot[0] = (task.rot[0] + 1) % k;
-                int i = 0;
-                while (task.rot[i] == 0 && i < nbiter){
-                    i++;
-                    task.rot[i] = (task.rot[i] + 1) %k;
-                }
-                
-                if (solve_isgood(&task)) {
-                    u64 DS640;
-                    u64 Y0;
-                	solve(&task, &DS640, &Y0);
-                    printf("candidat DS64 trouvé !!\n");
-                    printf("%llu\n", DS640);
-                    printf("temps pour trouver la solution = %f\n", wtime() - t1 );
-                }
-                /*if(DS640 == 7304601715607344736u){
-                    printf("On a le bon !\n");
-                    printf("DS640 = %llu\n", DS640);
-                }*/
-            }
-            finish_task(X, &task);
-        }
+    /* init all tasks */
+    int T = omp_get_max_threads();
+    struct task_t task[T];
+    #pragma omp parallel
+    {
+        int tid = omp_get_thread_num();
+        init_task(&task[tid]);
     }
+
+    u64 current = range_start;
+    while (current < range_end) {
+        #pragma omp parallel
+        {
+            int tid = omp_get_thread_num();
+            if (current + tid < range_end) {
+                do_task(current + tid, &task[tid], X);
+            }
+        }
+        // checkpoint();
+        current += T;
+    }
+
+
+    /*if(DS640 == 7304601715607344736u){
+        printf("On a le bon !\n");
+        printf("DS640 = %llu\n", DS640);
+    }*/
+
     printf("temps total = %f\n", wtime() - t1);
     return(0);
 }
